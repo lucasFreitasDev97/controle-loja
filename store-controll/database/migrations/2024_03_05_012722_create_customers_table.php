34006330a3ebb740b368e81e238cbe2a89234516
lucasFreitasDev97 <lukasm2@live.com>
2024-03-06 20:26:08 -0300

    add birth_date on table customers

--- store-controll/database/migrations/2024_03_05_012722_create_customers_table.php
+++ store-controll/database/migrations/2024_03_05_012722_create_customers_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->date('birth_date')->nullable();
             $table->string('email');
             $table->string('phone');
             $table->string('cpf');
